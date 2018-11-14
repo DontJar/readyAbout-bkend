2beedf6e6390c89a94934063d380a8f56b75f6ea
github username <github email address>
2018-11-14 13:36:49 -0500

    Revert "OAuth for google attempt number 1"
    This reverts commit 9f3978021cf56f2803239baaf54386743641e10c.

--- config/initializers/omniauth.rb
+++
@@ -1,5 +0,0 @@
-OmniAuth.config.logger = Rails.logger
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '1020525534277-nvd2ljrljsaq2571ueeebfjl09qf6ngf.apps.googleusercontent.com', 'RLnHJUTAXniiBS5UaUlKFG_O', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
   resources :users, :boats, :trips, :logbooks, :logs
-  mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
-  post 'auth/request', to:'authorization#get_authorization'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
