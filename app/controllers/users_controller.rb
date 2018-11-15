352a14c3b09851219a482c5f4f267d0a33129242
github username <github email address>
2018-11-15 10:55:56 -0500

    New user creation using JWT-Auth working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,36 +1,37 @@
 class UsersController < ApplicationController
-  before_action :find_user, only: [:update]
+  skip_before_action :authorized, only: %i[create]
 
-  def index
-    @users = User.all
-    render json: @users
+def create
+  @user = User.create(user_params)
+  if @user.valid?
+    @token = encode_token(user_id: @user.id)
+    render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
+  else
+    render json: { error: 'failed to create user' }, status: :not_acceptable
   end
+end
 
-  def create
-      @user = User.create(user_params)
-      if @user.valid?
-        render json: {user: UserSerializer.new(@user)}, status: :created
-      else
-        render json: {error: 'failed to create user'}, status: :not_accepted
-      end
-    end
+  # def index
+  #   @users = User.all
+  #   render json: @users
+  # end
 
-  def show
-    render json: User.find(params[:id]), include: ['boats']
-  end
+  # def show
+  #   render json: User.find(params[:id]), include: ['boats']
+  # end
 
-  def update
-    @user.update(user_params)
-    if @user.save
-      render json: @user, status: :accepted
-    else
-      render json: { errors: @user.errors.full_messages }, status: :unprocessible_entity
-    end
-  end
+  # def update
+  #   @user.update(user_params)
+  #   if @user.save
+  #     render json: @user, status: :accepted
+  #   else
+  #     render json: { errors: @user.errors.full_messages }, status: :unprocessible_entity
+  #   end
+  # end
 
-  def destroy
-    render json: User.find(params[:id]).destroy
-  end
+  # def destroy
+  #   render json: User.find(params[:id]).destroy
+  # end
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :boats, :trips, :logbooks, :logs, :users, only: %i[create]
+  resources :boats, :trips, :logbooks, :logs
+  resources :users, only: %i[create]
   post '/login', to: 'auth#create'
   get '/profile', to: 'user#profile'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
